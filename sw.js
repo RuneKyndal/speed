9fa098ec12f3adb62dd96e0b0aad9e2073748de3
RuneKyndal <rune@kyndal.dk>
2026-04-18 00:05:40 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,45 @@
-const CACHE_NAME = 'speed-v1.81';
+const CACHE_NAME = 'speed-v1.84';
 const ASSETS = [
   './',
   './index.html',
   './manifest.json',
-  './sw.js',
-  './speed_icon.png'
+  './speed_icon.png',
+  './sw.js'
 ];
 
+// Install: Cache everything
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching assets for offline use');
+      return cache.addAll(ASSETS);
+    })
   );
   self.skipWaiting();
 });
 
+// Activate: Delete old versions of the app
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
-      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            console.log('Deleting old cache:', key);
+            return caches.delete(key);
+          }
+        })
+      );
     })
   );
   self.clients.claim();
 });
 
+// Fetch: Serve from cache first, then network
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
 });
